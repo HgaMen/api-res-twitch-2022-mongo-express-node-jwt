7d269eab78ccd41e02e1e650db30a41f42955265
HgaMen <hga.int2021@gmail.com>
2022-07-06 12:13:02 -0500

    pendiente patch

--- middlewares/requiereToken.js
+++ middlewares/requiereToken.js
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken';
+import { tokenVerificationErrors } from '../utils/tokenManager.js';
+
 export const requiereToken = (req, res, next) => {
   try {
     let token = req.headers?.authorization;
@@ -13,17 +15,8 @@ export const requiereToken = (req, res, next) => {
     next();
   } catch (error) {
     console.log(error);
-
-    const TokenVerificationErrors = {
-      'invalid signature': 'La firma del JWT no es válida',
-      'jwt expired': 'JWT expirado',
-      'invalid token': 'Token no válido',
-      'No Bearer': 'Utiliza formato Bearer',
-      'jwt malformed': 'JWT formato no válido',
-    };
-
     return res
       .status(401)
-      .send({ error: TokenVerificationErrors[error.message] });
+      .send({ error: tokenVerificationErrors[error.message] });
   }
 };
